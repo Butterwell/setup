2e59c32c7e9197d934fb364434a2ec0c4581c533
Christopher Michael Buck <butterwell@gmail.com>
2013-10-23 18:19:39 -0400

    Add mongodb install

--- kitchen-sink.sh
+++ kitchen-sink.sh
@@ -3,7 +3,15 @@
 # for headless setup.  Assume git, curl, and npm are already installed.
 
 sudo apt-get install -y redis-server
-npm install -g derby
+
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+sudo service mongodb start
+
+npr install -g derby
 
 # TODO Serve default derby apps
 # TODO Serve my new development app ... as soon as it's written
